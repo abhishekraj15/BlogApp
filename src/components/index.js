37264c4d4ac76503b266331d010cef739f816a01
abhishekraj15 <rabhi0766@gmail.com>
2024-08-04 09:25:07 +0530

    update

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Select from "./Select";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBtn from "./Head/LogoutBtn";
+import { LogoutBtn } from "./Head/LogoutBtn";
 import RTE from "./RTE";
 import Login from "./Login";
 import Signup from "./Singup";
@@ -13,18 +13,18 @@ import AuthLayout from "./AuthLayout";
 import Input from "./Input";
 import Head from "./Head/Head";
 export {
-    Head,
-    Footer,
-    Container,
-    Logo,
-    LogoutBtn,
-    RTE,
-    Signup,
-    Login,
-    Button,
-    PostForm,
-    PostCard,
-    AuthLayout, 
-    Input,
-    Select,
-}
+  Head,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
+  Select,
+};
